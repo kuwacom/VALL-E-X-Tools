a53f27c42b9d6c7ba6b70e92a864a16a4511c01b
kuwacom <kuwa.com3@gmail.com>
2024-01-30 23:29:14 +0900

    init

---
+++ test.py
@@ -0,0 +1,18 @@
+from utils.generation import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+from IPython.display import Audio
+
+# download and load all models
+preload_models()
+
+# generate audio from text
+text_prompt = """
+これ新しい読み上げAIだよ
+"""
+audio_array = generate_audio(text_prompt)
+
+# save audio to disk
+write_wav("vallex_generation.wav", SAMPLE_RATE, audio_array)
+
+# play text in notebook
+Audio(audio_array, rate=SAMPLE_RATE)
